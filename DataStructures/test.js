ecf47d2d50304e2f845882a0a27e165cc96f9561
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2023-12-07 20:37:45 +0530

    test

---
+++ DataStructures/test.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.reverse()
+console.log(fruits)
+
+const numbers = [23, 100, 470, 9];
+numbers.sort((a,b) => {return a-b})
+console.log(numbers)
